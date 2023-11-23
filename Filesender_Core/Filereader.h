d5f3ee2c301e04809fcab4c084abe8e4ce2fbe22
HLammons1 <HLammons1@cnm.edu>
2023-11-22 17:21:58 -0700

    added filereader.h, filereader.cpp

---
+++ Filesender_Core/Filereader.h
@@ -0,0 +1,16 @@
+#ifndef _FILEREADER_H
+#define _FILEREADER_H
+
+#include <string>
+#include <fstream>
+
+class Filereader {
+private:
+
+public:
+
+};
+
+//state pattern for encryption state or decryption state
+//proxy pattern
+#endif // !_FILEREADER_H
